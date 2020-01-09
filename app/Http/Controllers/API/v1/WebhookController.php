1182cdd7ef1d2ef5220f316b0cefe61c51c08d6d
Damian Puister <damian.puister@beeproger.com>
2020-01-09 08:50:15 +0100

    Changes the dates

--- app/Http/Controllers/API/v1/WebhookController.php
+++ app/Http/Controllers/API/v1/WebhookController.php
@@ -36,14 +36,14 @@ class WebhookController extends Controller
         $user = User::where('rebase_user_id', $baseuser->AfspraakInfo->Client_Uniek_ID)->first();
 
         if($baseuser->AfspraakInfo->Afspraakstatus === 1 && $user) {
-            $user->tokens->map(function ($token) {
-                (new SendMessage())->sendFCM('Er is een niewe afspraak', 'Nieuwe afspraak', $token->push_token);
+            $user->tokens->map(function ($token) use ($baseuser) {
+                (new SendMessage())->sendFCM('Er is een niewe afspraak op '. $baseuser->AfspraakInfo->Datum, 'Nieuwe afspraak', $token->push_token);
             });
             return new JsonResponse($request->get('key'));
         }
 
-        $user->tokens->map(function ($token) {
-            (new SendMessage())->sendFCM('Er is een een afspraak gewijzigd op '. Carbon::now()->toDateString(), 'Gewijzigde afspraak', $token->push_token);
+        $user->tokens->map(function ($token) use ($baseuser) {
+            (new SendMessage())->sendFCM('Er is een een afspraak gewijzigd op '. $baseuser->AfspraakInfo->Datum, 'Gewijzigde afspraak', $token->push_token);
         });
 
         return new JsonResponse($request->get('key'));
