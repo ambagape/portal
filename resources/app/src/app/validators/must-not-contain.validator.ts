f26eb734b21a5a138170559e4e375dd65a9539ff
Martijn Wagena <martijn.wagena@beeproger.com>
2021-05-05 10:35:44 +0200

    specific pattern validation. make the + forbidden

---
+++ resources/app/src/app/validators/must-not-contain.validator.ts
@@ -0,0 +1,18 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export const mustNotContainValidator = (
+    blacklist: string ,
+    error: ValidationErrors
+): ValidatorFn => {
+    return (control: AbstractControl): { [key: string]: any } => {
+        if (!control.value) {
+            // if control is empty return no error
+            return null;
+        }
+
+        const valid = control.value.indexOf(blacklist) === -1;
+
+        // if true, return no error (no error), else return error passed in the second parameter
+        return valid ? null : error;
+    };
+};
---
+++ resources/app/src/app/validators/password-match.validator.ts
@@ -0,0 +1,11 @@
+import { AbstractControl } from "@angular/forms";
+
+export const passwordMatchValidator = (control: AbstractControl) => {
+    const password: string = control.get('password').value; // get password from our password form control
+    const confirmPassword: string = control.get('confirm').value; // get password from our confirmPassword form control
+    // compare is the password math
+    if (password !== confirmPassword) {
+      // if they don't match, set an error in our confirmPassword form control
+      control.get('confirm').setErrors({ NoPassswordMatch: true });
+    }
+  }
